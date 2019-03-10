6edfaf21eb7888da5c30c9381de2a3b84dddd07f
Michela Burns <michelaburns@berkeley.edu>
2019-03-10 16:46:44 -0700

    Fix getAllMessages because the new Message had the wrong parameters

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -84,42 +84,41 @@ public class Datastore {
 
 
 
-	public List<Message> getAllMessages() {
-		List<Message> messages = new ArrayList<>();
+  /** Returns the total number of messages for all users. */
+  public int getTotalMessageCount(){
+    Query query = new Query("Message");
+    PreparedQuery results = datastore.prepare(query);
+    return results.countEntities(FetchOptions.Builder.withLimit(1000));
+  }
 
-		Query query = new Query("Message")
-				.addSort("timestamp", SortDirection.DESCENDING);
-		PreparedQuery results = datastore.prepare(query);
+  /* Fetches messages for all users. */
+  public List<Message> getAllMessages(){
+  List<Message> messages = new ArrayList<>();
 
-		for (Entity entity : results.asIterable()) {
-			try {
-				String idString = entity.getKey().getName();
-				UUID id = UUID.fromString(idString);
-				String user = (String) entity.getProperty("user");
-				String text = (String) entity.getProperty("text");
-				long timestamp = (long) entity.getProperty("timestamp");
+  Query query = new Query("Message")
+    .addSort("timestamp", SortDirection.DESCENDING);
+  PreparedQuery results = datastore.prepare(query);
 
-				Message message = new Message(id, user, text, timestamp);
-				messages.add(message);
-			} catch (Exception e) {
-				System.err.println("Error reading message.");
-				System.err.println(entity.toString());
-				e.printStackTrace();
-			}
-		}
+  for (Entity entity : results.asIterable()) {
+   try {
+    String idString = entity.getKey().getName();
+    UUID id = UUID.fromString(idString);
+    String user = (String) entity.getProperty("user");
+    String text = (String) entity.getProperty("text");
+    long timestamp = (long) entity.getProperty("timestamp");
+    String recipient = (String) entity.getProperty("recipient");
 
-		return messages;
-	}
+    Message message = new Message(id, user, text, timestamp, recipient);
+    messages.add(message);
+   } catch (Exception e) {
+    System.err.println("Error reading message.");
+    System.err.println(entity.toString());
+    e.printStackTrace();
+   }
+  }
 
+  return messages;
+ }
 
-	/**
-	 * Returns the total number of messages for all users.
-	 */
-	public int getTotalMessageCount() {
-		Query query = new Query("Message");
-		PreparedQuery results = datastore.prepare(query);
-		return results.countEntities(FetchOptions.Builder.withLimit(1000));
-	}
-	
 }
 
