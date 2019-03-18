c597bc965a23381c873b146eae4f9b31ab35848d
Rabab Ibrahim <rababi@uw.edu>
2019-03-18 16:15:46 -0700

    added styled text part 2 code

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -95,5 +95,6 @@ function buildUI() {
   setPageTitle();
   showMessageFormIfLoggedIn();
   fetchMessages();
+  fetchAboutMe();
   ClassicEditor.create( document.getElementById('message-input') );
 }
