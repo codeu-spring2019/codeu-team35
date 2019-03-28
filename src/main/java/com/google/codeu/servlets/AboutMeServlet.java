e9531b09adb33a277092a0a3374729d4db2040e4
Matt Silverlock <matt@eatsleeprepeat.net>
2019-03-28 08:33:57 -0700

    about.html -> about

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -31,21 +31,20 @@ public class AboutMeServlet extends HttpServlet {
    * Responds with the "about me" section for a particular user.
    */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     response.setContentType("text/html");
 
     String user = request.getParameter("user");
 
-    if(user == null || user.equals("")) {
+    if (user == null || user.equals("")) {
       // Request is invalid, return empty response
       return;
     }
 
     User userData = datastore.getUser(user);
 
-    if(userData == null || userData.getAboutMe() == null) {
+    if (userData == null || userData.getAboutMe() == null) {
       return;
     }
 
@@ -53,8 +52,7 @@ public class AboutMeServlet extends HttpServlet {
   }
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     UserService userService = UserServiceFactory.getUserService();
     if (!userService.isUserLoggedIn()) {
@@ -64,7 +62,7 @@ public class AboutMeServlet extends HttpServlet {
 
     String userEmail = userService.getCurrentUser().getEmail();
     String aboutMe = Jsoup.clean(request.getParameter("about-me"), Whitelist.none());
-    //String aboutMe = request.getParameter("about-me");
+    // String aboutMe = request.getParameter("about-me");
 
     User user = new User(userEmail, aboutMe);
     datastore.storeUser(user);
